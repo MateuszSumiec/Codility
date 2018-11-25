c0a7097bbbf8a5b6c2dfe6113d209b5daae6339b
MateuszSumiec <msumiec@gmail.com>
2018-11-25 19:32:30 +0100

    Update binary_gap.py

--- BinaryGap/BinaryGap_python/binary_gap.py
+++ BinaryGap/BinaryGap_python/binary_gap.py
@@ -1,6 +1,16 @@
-def solution(A, K):
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 13:53:06 2018
+Edited on Sun Nov 25 16:42 2018
+@author: Mateusz
+"""
+
+def solution(N):
     # write your code in Python 3.6
-    if A:
-        return A[(len(A)-K) % len(A):] + A[:(len(A)-K) % len(A)]
-    else:
-        return A
+    binary = bin(N)[2:].split('1')
+    
+    if (binary[-1] != ''):
+        binary.remove(binary[-1])
+    
+    return len(max(binary))
+
