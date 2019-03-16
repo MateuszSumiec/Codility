6a2ecd453692b449223c611fe6f5bf33ddab77a2
MateuszSumiec <msumiec@gmail.com>
2019-03-16 11:22:34 +0100

    Add files via upload

---
+++ Lesson 3 Time Complexity/tape_equilibrium.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 23:01:01 2019
+
+@author: Mateusz
+"""
+
+
+def solution(A):
+    # write your code in Python 3.6
+    if A:
+        suma = A[0] - sum(A[1:])
+        best = abs(suma)
+        for i in range(len(A) - 2):
+            suma += 2 * A[i+1]
+            if abs(suma) <= abs(best):
+                best = suma
+        return abs(best)
+    else:
+        return -1
+
+
+A = [3, 1, 2, 4, 3]
+print(solution(A))
