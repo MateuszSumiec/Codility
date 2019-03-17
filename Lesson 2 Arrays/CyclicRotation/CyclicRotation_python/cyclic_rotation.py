62f54c3fb1399ecdacba2edf417a89d28b44d4a6
MateuszSumiec <msumiec@gmail.com>
2019-03-17 16:04:21 +0100

    Update docstring

--- Lesson 2 Arrays/CyclicRotation/CyclicRotation_python/cyclic_rotation.py
+++ Lesson 2 Arrays/CyclicRotation/CyclicRotation_python/cyclic_rotation.py
@@ -1,4 +1,9 @@
 def solution(A, K):
+    """
+    Rotate an array A to the right by given number of steps K.
+    len(A) and K are integers within the range [0..100];
+    Each element of array A is an integer within the range [−1 000..1 000].
+    """
     if A:
         return A[(len(A)-K) % len(A):] + A[:(len(A)-K) % len(A)]
     else:
