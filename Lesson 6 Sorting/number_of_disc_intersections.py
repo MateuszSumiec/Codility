a11083703e10bd522490c6d5db762726b4c02d89
Mateusz <msumiec@gmail.com>
2019-03-17 18:56:23 +0100

    Initialize with docstring

---
+++ Lesson 6 Sorting/number_of_disc_intersections.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 18:45:03 2019
+
+@author: Mateusz
+"""
+
+
+def solution(A):
+    """
+    Returns number of intersecting discs with different centers.
+    A[k] is a radius in [0, ..., 2 147 483 647]
+    for disc k in [0, ..., 100 000].
+    """
+    pass
+
+
+assert solution([1, 5, 2, 1, 4, 0]) == 11, "zle liczysz"
