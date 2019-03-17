b21dc4cc4dd88a162d4c8b9106f2964f3ed7decb
Mateusz <msumiec@gmail.com>
2019-03-17 17:55:18 +0100

    Initilize with a docstring

---
+++ Lesson 6 Sorting/triangle.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 17:49:14 2019
+
+@author: Mateusz
+"""
+
+
+def solution(A):
+    """
+    Returns 1 if in A exist triangle (3 numbers)
+    that can be constructed, 0 otherwise.
+
+    A[k] is an integer within the range [−2,147,483,648..2,147,483,647],
+    for k in [0..100,000]
+    """
+    pass
