7f3ef105a49ba38b9fc7a6b8f29d37793e05fb24
Mateusz <msumiec@gmail.com>
2019-03-17 17:03:56 +0100

    Init distinct

---
+++ Lesson 6 Sorting/distinct.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 16:03:24 2019
+
+@author: Mateusz
+"""
+
+
+def solution(A):
+    """
+    Returns distinct values of array A.
+
+    A[k] in [−1 000 000, ..., 1 000 000] for k in [0..100,000].
+    """
+    return sorted(A)
+
+
+A = [2, 1, 1, 2, 3, 1]
+print(solution(A))
