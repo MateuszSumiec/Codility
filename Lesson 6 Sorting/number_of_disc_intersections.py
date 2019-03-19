8752441769d9f8990b716d7cec46238ae75ad70c
Mateusz <msumiec@gmail.com>
2019-03-19 19:11:15 +0100

    50% evaluation

--- Lesson 6 Sorting/number_of_disc_intersections.py
+++ Lesson 6 Sorting/number_of_disc_intersections.py
@@ -12,7 +12,19 @@ def solution(A):
     A[k] is a radius in [0, ..., 2 147 483 647]
     for disc k in [0, ..., 100 000].
     """
-    pass
+    count = 0
+    for i in range(len(A)):
+        for j in range(i+1, len(A)):
+            print('i(', i-A[i], i+A[i], ')')
+            print('j(', j-A[j], j+A[j], ')')
+            print(i-A[i], j-A[j], j+A[j], i+A[i])
+            if (j-A[j] <= i-A[i] <= j+A[j]) or (j-A[j] <= i+A[i] <= j+A[j]) or ((i-A[i] <= j-A[j]) and (j+A[j] <= i+A[i])):
+                count += 1
 
-print('lalalala')
-assert solution([1, 5, 2, 1, 4, 0]) == 11, "zle liczysz"
+            print(count)
+    return count
+
+
+B = [1,1,1]
+
+print('sol B ', solution(B))
