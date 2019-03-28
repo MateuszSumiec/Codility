4f4eaed08adc1b937454de3751bac26e122e0dfd
MateuszSumiec <msumiec@gmail.com>
2019-03-28 23:46:28 +0100

    Update max_product_of_three.py

--- Lesson 6 Sorting/max_product_of_three.py
+++ Lesson 6 Sorting/max_product_of_three.py
@@ -11,7 +11,7 @@ def solution(A):
     Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
 
     A[k] is an integer within the range [−1 000, ..., 1 000]
-    for k in in [3..100,000].
+    for k in in [3, ..., 100 000].
     """
     if len(A) > 2:
         sorted_A = sorted(A)
